5227b3a5dfe082bc27161f347c0ad3196a975451
Ville Juutilainen <vjuutilainen@gmail.com>
2016-04-13 14:35:17 +0300

    mockup data

--- js/beef-vis.js
+++ js/beef-vis.js
@@ -1,5 +1,38 @@
 $.extend(beefApp, {
   initVis: function () {
-    console.log(d3);
+    
+    var data = [
+      {
+        sentence_id: 1,
+        count: 4,
+        sentence: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      },
+      {
+        sentence_id: 2,
+        count: 5,
+        sentence: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        sentence_id: 3,
+        count: 9,
+        sentence: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      },
+      {
+        sentence_id: 4,
+        count: 15,
+        sentence: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      },
+      {
+        sentence_id: 5,
+        count: 20,
+        sentence: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      },
+      {
+        sentence_id: 6,
+        count: 12,
+        sentence: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+      }
+    ];   
+
   }
 });
